14da6690914b3c75a47253ea31f32b03b091315d
GaryGuo <garyriot@gmail.com>
2016-08-02 14:30:14 +0800

    rm recomposer

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # Ensure migration is performed and database is not empty.
-service mysql restart
 if [[ ! -e migrated ]]; then
     php artisan migrate
-
-	composer install
 
     touch migrated
 fi
